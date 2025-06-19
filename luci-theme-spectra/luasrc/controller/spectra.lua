94eaf050a3a35e7964cc1034d477203be3579c1f
yufanpin <3281107880@qq.com>
2025-06-19 23:27:50 +0800

    Delete luci-theme-spectra directory

--- luci-theme-spectra/luasrc/controller/spectra.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.spectra", package.seeall)
-
-function index()
-    entry({"admin", "services", "spectra"}, firstchild(), _("Spectra Config"), 1).leaf = false
-    entry({"admin", "services", "spectra", "index"}, template("spectra/index"), _("Home"), 2).leaf = true
-    entry({"admin", "services", "spectra", "filekit"}, template("spectra/filekit"), _("Manager"), 3).leaf = true
-end
